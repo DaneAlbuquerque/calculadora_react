9efc350b125eb260417be01ef30e83c300618d1c
Dayane Albuquerque <dayalb_pas@hotmail.com>
2026-05-14 15:42:51 -0300

    Calculadora React

--- src/App.js
+++ src/App.js
@@ -1,102 +1,145 @@
+import Input from "./components/Input";
+import Button from "./components/Button";
 
-import Input from './components/Input';
-import Button from './components/Button';
-
-import { Container, Content, Row } from './styles';
-import { useState } from 'react';
+import { Container, Content, Row } from "./styles";
+import { useState } from "react";
 
 
 const App = () => {
-  const [currentNumber, setCurrentNumber] = useState('0');
-  const [firstNumber, setFirstNumber] = useState('0');
-  const [operation, setOperation] = useState('');
+  const [currentNumber, setCurrentNumber] = useState("0");
+  const [firstNumber, setFirstNumber] = useState("0");
+  const [operation, setOperation] = useState("");
 
   const handleOnClear = () => {
-    setCurrentNumber('0')
-    setFirstNumber('0')
-    setOperation('')
+    setCurrentNumber("0");
+    setFirstNumber("0");
+    setOperation("");
   };
 
   const handleAddNumber = (num) => {
-    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${num}`)
-  }
+  setCurrentNumber((prev) => {
+    if (prev === "0") {
+      return num;
+    }
+
+    return prev + num;
+  });
+};
 
   const handleSumNumbers = () => {
-
-    if(firstNumber === '0'){
-        setFirstNumber(String(currentNumber));
-        setCurrentNumber('0')
-        setOperation('+')
-    }else {
+    if (firstNumber === "0") {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber("0");
+      setOperation("+");
+    } else {
       const sum = Number(firstNumber) + Number(currentNumber);
-      setCurrentNumber(String(sum))
-      setOperation('')
+      setCurrentNumber(String(sum));
+      setOperation("");
     }
-
-  }
+  };
 
   const handleMinusNumbers = () => {
-
-    if(firstNumber === '0'){
-        setFirstNumber(String(currentNumber));
-        setCurrentNumber('0')
-        setOperation('-')
-    }else {
+    if (firstNumber === "0") {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber("0");
+      setOperation("-");
+    } else {
       const sum = Number(firstNumber) - Number(currentNumber);
-      setCurrentNumber(String(sum))
-      setOperation('')
+      setCurrentNumber(String(sum));
+      setOperation("");
     }
-
-  }
+  };
 
   const handleEquals = () => {
+    if (firstNumber !== "0" && operation !== "" && currentNumber !== "0") {
+      switch (operation) {
+        case "+":
+          handleSumNumbers();
+          break;
+        case "-":
+          handleMinusNumbers();
+          break;
+        case "*":
+          handleMultiplication();
+          break;
+        case "/":
+          handleDivision();
+          break;
 
-    if(firstNumber !== '0' && operation !== '' && currentNumber !== '0'){
-        switch(operation){
-          case '+':
-            handleSumNumbers();
-            break;
-          case '-':
-            handleMinusNumbers();
-            break;
-          default: 
-            break;
-        }
+        default:
+          break;
+      }
     }
+    };
 
+ const handleMultiplication = () => {
+      if (firstNumber === "0") {
+        setFirstNumber(String(currentNumber));
+        setCurrentNumber("0");
+        setOperation("*");
+      } else {
+        const multiplication = Number(firstNumber) * Number(currentNumber);
+        setCurrentNumber(String(multiplication));
+        setOperation("");
+      }
+    };
+
+  const handleDivision = () => {
+  if (firstNumber === "0") {
+    setFirstNumber(String(currentNumber));
+    setCurrentNumber("0");
+    setOperation("/");
+  } else {
+    const division = Number(firstNumber) / Number(currentNumber);
+    setCurrentNumber(String(division));
+    setOperation("");
   }
+};
 
+  const handlePercentage = () => {
+  const percentage = Number(currentNumber) / 100;
+  setCurrentNumber(String(percentage));
+};
+  
   return (
-    <Container>
-      <Content>
-        <Input value={currentNumber}/>
-        <Row>
-          <Button label="x"/>
-          <Button label="/"/>
-          <Button label="c" onClick={handleOnClear}/>
-          <Button label="."/>
-        </Row>
-        <Row>
-          <Button label="7" onClick={() => handleAddNumber('7')}/>
-          <Button label="8" onClick={() => handleAddNumber('8')}/>
-          <Button label="9" onClick={() => handleAddNumber('9')}/>
-          <Button label="-" onClick={handleMinusNumbers}/>
-        </Row>
-        <Row>
-          <Button label="4" onClick={() => handleAddNumber('4')}/>
-          <Button label="5" onClick={() => handleAddNumber('5')}/>
-          <Button label="6" onClick={() => handleAddNumber('6')}/>
-          <Button label="+" onClick={handleSumNumbers}/>
-        </Row>
-        <Row>
-          <Button label="1" onClick={() => handleAddNumber('1')}/>
-          <Button label="2" onClick={() => handleAddNumber('2')}/>
-          <Button label="3" onClick={() => handleAddNumber('3')}/>
-          <Button label="=" onClick={handleEquals}/>
-        </Row>
-      </Content>
-    </Container>
-  );
-}
+    <div className="bg-zinc-950 h-screen flex items-center justify-center">
+      <div className="bg-zinc-900 p-6 rounded-3xl shadow-2xl w-80">
+        
+       <input
+         type="text"
+         value={currentNumber}
+         readOnly
+         className="w-full mb-4 bg-zinc-800 text-white text-right text-4xl p-4 rounded-2xl"
+/>
+
+        <div className="grid grid-cols-4 gap-3">
+
+          <button onClick={handleOnClear} className="bg-orange-500 text-white p-4 rounded-2xl">C</button>
+          <button onClick={handlePercentage} className="bg-orange-500 text-white p-4 rounded-2xl">%</button>
+          <button onClick={handleDivision} className="bg-orange-500 text-white p-4 rounded-2xl">/</button>
+          <button onClick={handleMultiplication} className="bg-orange-500 text-white p-4 rounded-2xl">x</button>
+     
+          <button onClick={() => handleAddNumber("7")} className="bg-zinc-700 text-white p-4 rounded-2xl">7</button>
+          <button onClick={() => handleAddNumber("8")} className="bg-zinc-700 text-white p-4 rounded-2xl">8</button>
+          <button onClick={() => handleAddNumber("9")} className="bg-zinc-700 text-white p-4 rounded-2xl">9</button>
+          <button onClick={handleMinusNumbers} className="bg-orange-500 text-white p-4 rounded-2xl">-</button>
+
+          <button onClick={() => handleAddNumber("4")} className="bg-zinc-700 text-white p-4 rounded-2xl">4</button>
+          <button onClick={() => handleAddNumber("5")} className="bg-zinc-700 text-white p-4 rounded-2xl">5</button>
+          <button onClick={() => handleAddNumber("6")} className="bg-zinc-700 text-white p-4 rounded-2xl">6</button>
+          <button onClick={handleSumNumbers} className="bg-orange-500 text-white p-4 rounded-2xl">+</button>
+
+         <button onClick={() => handleAddNumber("1")} className="bg-zinc-700 text-white p-4 rounded-2xl">1</button>
+         <button onClick={() => handleAddNumber("2")} className="bg-zinc-700 text-white p-4 rounded-2xl">2</button>
+         <button onClick={() => handleAddNumber("3")} className="bg-zinc-700 text-white p-4 rounded-2xl">3</button>
+        <button onClick={handleEquals} className="bg-orange-500 text-white p-4 rounded-2xl">=</button>
+
+        <button onClick={() => handleAddNumber("0")} className="bg-zinc-700 text-white p-4 rounded-2xl col-span-4">0</button>
+          
+        </div>
+      </div>
+    </div>
+    );
+};
 
 export default App;
